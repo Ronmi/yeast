87bbc00658261597bbd58128f66845863707bf71
Ronmi Ren <ronmi.ren@gmail.com>
2016-12-19 20:29:07 +0800

    Now can lock manage page with password

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net/http"
 	"os/exec"
+
+	"github.com/Patrolavia/toolkit/session"
 )
 
 func main() {
@@ -17,12 +19,14 @@ func main() {
 		port   string
 		ngconf string
 		fend   string
+		pass   string
 		debug  bool
 	)
 	flag.StringVar(&data, "data", "/var/lib/cheesecake/data.json", "path to store mapping")
 	flag.StringVar(&port, "addr", ":8080", "address to listen")
 	flag.StringVar(&ngconf, "conf", "/etc/nginx/sites-enabled/default", "path to nginx config")
-	flag.StringVar(&fend, "fe", "index.html", "Path to frontend file")
+	flag.StringVar(&fend, "fe", ".", "Path to directory holding frontend files")
+	flag.StringVar(&pass, "pass", "", "password to lock the manage page")
 	flag.BoolVar(&debug, "debug", false, "debug mode")
 	flag.Parse()
 
@@ -31,9 +35,9 @@ func main() {
 		log.Fatalf("Cannot load data from %s: %s", data, err)
 	}
 
-	tmpl, err := ioutil.ReadFile(fend)
+	tmpl, err := ioutil.ReadFile(fend + "/index.html")
 	if err != nil {
-		log.Fatalf("Cannot read frontend file from %s: %s", fend, err)
+		log.Fatalf("Cannot read index page from %s/index.html: %s", fend, err)
 	}
 
 	f := func() bool {
@@ -57,9 +61,45 @@ func main() {
 	http.HandleFunc("/api/delete", h.Delete)
 	http.HandleFunc("/api/enable", h.Enable)
 	http.HandleFunc("/api/disable", h.Disable)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(tmpl))
-	})
+
+	rootHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write(tmpl)
+	}
+	realRoot := rootHandler
+	loginPage, err := ioutil.ReadFile(fend + "/login.html")
+	if err != nil {
+		log.Fatalf("Cannot read login page from %s/login.html: %s", fend, err)
+	}
+
+	if pass != "" {
+		realRoot = (&session.Middleware{
+			Manager: &session.Manager{},
+			Handler: func(w http.ResponseWriter, r *http.Request) {
+				sess := r.Context().Value("session").(*session.Session)
+				if sess == nil {
+					// no session, show login page
+					w.Write(loginPage)
+					return
+				}
+				defer sess.Save(w, session.DefaultCookieMaker)
+
+				if sess.Data() == "ok" {
+					rootHandler(w, r)
+					return
+				}
+				r.ParseForm()
+				if r.PostFormValue("pass") != pass {
+					// incorrect password, show login page
+					w.Write(loginPage)
+					return
+				}
+
+				sess.SetData("ok")
+				rootHandler(w, r)
+			},
+		}).Handle
+	}
+	http.HandleFunc("/", realRoot)
 
 	log.Fatal(http.ListenAndServe(port, nil))
 }
